6bbdcc3d60d21cc0688661cca5a385f13400ca6c
Anas Badr <anasihabezmc@gmail.com>
2023-09-09 17:39:04 +0300

    feat(app): added the option to draw arcs between two given countries (#27)
    * docs(app): Added my name to team members
    
    * feat(app): added a script to get svgs for all countries
    
    * feat(app): added a script to get svgs for all countries
    
    * fix(app): Removed unnamed folder and it's files
    
    * chore(poetry): updated project authors
    
    * docs(app): move contrib section to separate file
    
    * feat(GameManager): add manager skeleton structure
    
    * feat(GameManager): complete basic event loop
    
    * chore(pygame): use `pygame-ce`
    
    * feat(UI): add skeletal main menu
    
    * feat(UI): add handlers for main menu state
    
    * feat(UI): assign object_id to buttons
    
    * feat(World Map): add fully intractable world map (#21)
    
    * feat(UI): add screen system
    
    * feat(UI): add `main_menu` and `options_menu` screen
    
    * feat(game): add basic calls to draw world map on new game
    
    * style(WORLD MAP): fill countries with fg (`00394f`)
    
    * feat(World Map): add hover state check for countries
    
    * feat(world map): scale map only when required
    
    * feat(countries): add function to get random point on sprite(not working properly)
    
    * chore(poetry): mark extras dependencies group as optional
    
    * feat(app) added the option to draw arcs between two given countries
    
    ---------
    
    Co-authored-by: Rudra Vashishtha <91290891+rudravashishtha@users.noreply.github.com>
    Co-authored-by: p0lygun <solankivibhakar82@gmail.com>

--- game/screens/game/utils.py
+++ game/screens/game/utils.py
@@ -1,5 +1,8 @@
 import math
 
+import pygame as pg
+from countries import Country
+
 
 def pos2angle(A, B, aspectRatio):
     """Calculate the angle between two points."""
@@ -7,3 +10,24 @@ def pos2angle(A, B, aspectRatio):
     y = B[1] - A[1]
     angle = math.atan2(-y, x / aspectRatio)
     return angle
+
+
+def draw_arcs(countries: tuple[Country, Country], curve_height: int, num_segments: int, color: tuple[int, int, int],
+              screen: pg.Surface):
+    """Draw arcs between two countries."""
+    countries_points: list[list[int, int], list[int, int]] = []
+    for country in countries:
+        countries_points.append(country.get_random_point())
+
+    x1, y1 = countries_points[0]
+    x2, y2 = countries_points[1]
+
+    dx = (x2 - x1) / num_segments
+    dy = (y2 - y1) / num_segments
+
+    for i in range(num_segments + 1):
+        t = i / num_segments
+        x = x1 + dx * i
+        y = y1 + dy * i - curve_height * math.sin(t * math.pi)
+
+        pg.draw.circle(screen, color, (int(x), int(y)), 2)
